88ac494fdf92255a3805f8e2e898a5ccd057da3f
KUchy <krzysztofrudnicki0@gmail.com>
2021-08-05 17:14:02 +0200

    Adding miscelanious projects I have worked on for past years

---
+++ C/misc/generatingWordsEndingWIthalka.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+const int NUMBER_FOR_POLISH_SMALL_L = 136;
+
+int main()
+{
+    for(char i = 'a'; i < 'z' + 1; ++i) printf("%ca%cka\n", i, NUMBER_FOR_POLISH_SMALL_L);
+    return 0;
+}
---
+++ C/tests/generatingPolishLettersOnWindowsTerminal.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    printf("Henlo\n");
+    sleep(20);
+    return 0;
+}
