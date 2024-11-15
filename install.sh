9e22244297d1ff55a66cc9f68981b9463138dd11
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-11-15 14:31:15 +0100

    feat: install intel gpu tools only if intel gpu is being used

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-yes | sudo pacman -S --needed ttf-dejavu noto-fonts ttf-font-awesome bc intel-gpu-tools jq iw
-sudo setcap cap_perfmon+ep /usr/bin/intel_gpu_top
+
+# Check if Intel GPU is detected
+if lspci | grep -i 'vga' | grep -i 'intel'; then
+    yes | sudo pacman -S --needed intel-gpu-tools
+    sudo setcap cap_perfmon+ep /usr/bin/intel_gpu_top
+fi
+
+yes | sudo pacman -S --needed ttf-dejavu noto-fonts ttf-font-awesome bc jq iw
 cp -r i3blocks ~/.config/
 cp -r i3 ~/.config/
 i3-msg reload
