23acfdc98459c46a19d2c3f9bf1576f25dbd587d
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-11-17 11:38:47 +0100

    feat: make the script work on ubuntu

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
+# Function to detect if the system is Ubuntu
+is_ubuntu() {
+    [ -f /etc/os-release ] && grep -qi 'ubuntu' /etc/os-release
+}
+
 # Check if Intel GPU is detected
 if lspci | grep -i 'vga' | grep -i 'intel'; then
-    yes | sudo pacman -S --needed intel-gpu-tools
-    sudo setcap cap_perfmon+ep /usr/bin/intel_gpu_top
+    if is_ubuntu; then
+        sudo apt-get update
+        sudo apt-get install -y intel-gpu-tools
+        sudo setcap cap_perfmon+ep /usr/bin/intel_gpu_top
+    else
+        yes | sudo pacman -S --needed intel-gpu-tools
+        sudo setcap cap_perfmon+ep /usr/bin/intel_gpu_top
+    fi
 fi
 
-yes | sudo pacman -S --needed ttf-dejavu noto-fonts ttf-font-awesome bc jq iw
+if is_ubuntu; then
+    sudo apt-get update
+    sudo apt-get install -y fonts-dejavu-core fonts-noto ttf-font-awesome bc jq iw
+else
+    yes | sudo pacman -S --needed ttf-dejavu noto-fonts ttf-font-awesome bc jq iw
+fi
+
 cp -r i3blocks ~/.config/
 cp -r i3 ~/.config/
 i3-msg reload
