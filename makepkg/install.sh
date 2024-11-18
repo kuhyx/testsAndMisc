7b06f2558a6525e96f27c38240d2ab028bc9e34c
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2024-11-18 09:52:48 +0100

    feat: make packages and sources directory if not existing

--- makepkg/install.sh
+++ makepkg/install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+sudo mkdir -p /home/packages
+sudo mkdir -p /home/sources
+
+sudo chown $USER:$USER /home/packages
+chmod 755 /home/packages
+sudo chown $USER:$USER /home/sources
+chmod 755 /home/sources
+
 sudo cp makepkg.conf /etc/
