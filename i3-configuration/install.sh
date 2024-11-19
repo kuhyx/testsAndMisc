29e6dfb4cb7c25bb8b71fe7fbe4eb306c2cef35a
KRZYSZTOF RUDNICKI <krzysztofrudnicki0@gmail.com>
2024-11-19 06:41:47 +0100

    feat: add installation of pulse audio utils for ubuntu

--- i3-configuration/install.sh
+++ i3-configuration/install.sh
@@ -33,7 +33,7 @@ fi
 
 if is_ubuntu; then
     sudo apt-get update
-    sudo apt-get install -y fonts-dejavu-core fonts-noto fonts-font-awesome bc jq iw
+    sudo apt-get install -y fonts-dejavu-core fonts-noto fonts-font-awesome bc jq iw pulseaudio-utils
 else
     yes | sudo pacman -S --needed ttf-dejavu noto-fonts ttf-font-awesome bc jq iw acpi
 fi
