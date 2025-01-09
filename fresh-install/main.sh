5ea9a5c18b6da6305005068fcdb8d012cbf844ae
Krzysztof kuhy Rudnicki <krzysztofrudnicki0@gmail.com>
2025-01-09 20:50:21 +0100

    feat: updating packages installed in fresh install

--- fresh-install/main.sh
+++ fresh-install/main.sh
@@ -17,7 +17,7 @@ install_from_aur() {
     fi
     cd $(basename $repo_url .git)
     if ! pacman -Qi $pkg_name > /dev/null 2>&1; then
-        makepkg -A -i --nocheck --skipchecksums --skipinteg --skippgpcheck
+        yes | makepkg -A -i --nocheck --skipchecksums --skipinteg --skippgpcheck
     else
         echo "$pkg_name is already installed"
     fi
@@ -33,16 +33,7 @@ pacman_packages=(
     texlive-plaingeneric
     code
     docbook-xsl
-    python-build
-    python-installer
-    emacs-nox
-    pavucontrol
-    pbzip2
-    lzlib
-    re2c
-    ninja
-    libical
-    bluez
+    glu
     pavucontrol-qt
     mold
     zstd
@@ -62,6 +53,30 @@ pacman_packages=(
     bc
     acpi
     cargo
+    freeglut
+    texlive-latexextra
+    biber
+    texlive-bibtexextra
+    texlive-pictures
+    texlive-fontsextra
+    texlive-formatsextra
+    texlive-pstricks
+    texlive-games
+    texlive-humanities
+    texlive-science
+    node-gyp
+    plantuml
+    npm
+    ruby-ronn
+    go-tools
+    asciidoctor
+    man-db
+    git-lfs
+    nodejs
+    electron
+    yarn
+    openssl-1.1
+    tk
 )
 
 for pkg in "${pacman_packages[@]}"; do
@@ -79,14 +94,17 @@ done
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 
-# omz 
-
-
-
-
 aur_packages=(
+    "https://aur.archlinux.org/python2.git python2"
+    # "https://aur.archlinux.org/go-tools-git.git  go-tools-git"
+    # "https://aur.archlinux.org/git-lfs-git.git git-lfs-git"
+    # "https://aur.archlinux.org/nodejs-git.git nodejs-git"
+    # "https://aur.archlinux.org/jasper-git.git jasper-git"
+    "https://aur.archlinux.org/signal-desktop.git signal-desktop-beta"
+    "https://aur.archlinux.org/signal-account-switcher.git signal-account-switcher"
+    "https://aur.archlinux.org/libinih-git.git libinih-git"
     # "https://aur.archlinux.org/glibc-git.git glibc-git"
-    "https://aur.archlinux.org/gcc-git.git gcc-git"
+    # "https://aur.archlinux.org/gcc-git.git gcc-git"
     "https://aur.archlinux.org/plzip.git plzip"
     "https://aur.archlinux.org/zsh-git.git zsh"
     "https://aur.archlinux.org/visual-studio-code-bin.git visual-studio-code-bin"
@@ -97,12 +115,16 @@ aur_packages=(
     "https://aur.archlinux.org/rhash-git.git rhash"
     "https://aur.archlinux.org/cppdap-git.git cppdap"
     "https://aur.archlinux.org/bluez-git.git bluez-git"
-    "https://aur.archlinux.org/lynx-git.git lynx-git"iw
+    "https://aur.archlinux.org/lynx-git.git lynx-git"
     "https://aur.archlinux.org/pacman-git.git pacman-git"
     # "https://aur.archlinux.org/mold-git.git mold-git"
     "https://aur.archlinux.org/thorium-browser-bin.git thorium-browser"
+    "https://aur.archlinux.org/glu-git.git glu-git"
     "https://aur.archlinux.org/mupdf-git.git mupdf-git"
-    "https://aur.archlinux.org/nomacs-git.git nomacs-git"
+    "https://aur.archlinux.org/exiv2-git.git exiv2-git"
+    "https://aur.archlinux.org/libraw-git.git libraw-git"
+    "https://aur.archlinux.org/opencv-git.git opencv-git"
+    "https://aur.archlinux.org/nomacs-git.git nomacs-gitgit"
     "https://aur.archlinux.org/ffmpeg-full-git.git ffmpeg-full-git"
     "https://aur.archlinux.org/mpv-full-git.git mpv-full-git"
     "https://aur.archlinux.org/protontricks-git.git protontricks-git"
@@ -114,6 +136,9 @@ aur_packages=(
     "https://aur.archlinux.org/msvc-wine-git.git msvc-wine-git"
     "https://aur.archlinux.org/jq-git.git jq-git"
     "https://aur.archlinux.org/iw-git.git iw-git"
+    "https://aur.archlinux.org/deluge-git.git deluge-git"
+    "https://aur.archlinux.org/nvm-git.git nvm-git"
+    "https://aur.archlinux.org/unityhub-beta.git unityhub-beta"
 )
 
 for pkg in "${aur_packages[@]}"; do
@@ -128,3 +153,4 @@ else
     echo "Oh My Zsh is already installed"
 fi
 
+sudo pacman -R firefox
